072c3eef2472354b456fc4f1792a61b5e2234de1
Kristin <1358365@DESKTOP-4FH9G41>
2020-01-04 02:16:55 +0300

    it draws wierd ellipses

--- src/hello-world-tool.js
+++ src/hello-world-tool.js
@@ -1,9 +1,8 @@
 import csTools from "cornerstone-tools"
-import csCore from "cornerstone-core";
-
 const BaseBrushTool = csTools.importInternal("base/BaseBrushTool");
-const external = csTools.importInternal("externalModules");
+const external = csTools.getModule("externalModules");
 const segmentationModule = csTools.getModule('segmentation');
+const drawEllipse = csTools.importInternal("drawing/drawEllipse");
 
 export default class CountourFillTool extends BaseBrushTool {
   constructor(props = {}) {
@@ -19,10 +18,9 @@ export default class CountourFillTool extends BaseBrushTool {
 
     this.preMouseDownCallback = this.preMouseDownCallback.bind(this);
     this._drawingMouseUpCallback = this._drawingMouseUpCallback.bind(this);
-    this.proceedCalculations = this.proceedCalculations.bind(this);
-    this.draw = this.draw.bind(this);
     this.init = this.init.bind(this);
     this.renderBrush = this.renderBrush.bind(this);
+    this.mouseDragCallback = this.mouseDragCallback.bind(this);
      }
 
      init(evt){
@@ -32,7 +30,7 @@ export default class CountourFillTool extends BaseBrushTool {
        this.rows = eventData.image.rows;
        this.columns = eventData.image.columns;
 
-       const { configuration, getters } = segmentationModule;
+       const {getters } = segmentationModule;
 
        const {
          labelmap2D,
@@ -53,17 +51,19 @@ export default class CountourFillTool extends BaseBrushTool {
        };
      }
 
-       preMouseDownCallback(evt) {
+      preMouseDownCallback(evt) {
       const eventData = evt.detail;
 
       this.init(evt);
 
-      const {element, currentPoints } = eventData;
+      const {element, currentPoints} = eventData;
 
       this.startCoords = currentPoints.image;
 
       this._drawing = true;
       super._startListeningForMouseUp(element);
+      this._lastImageCoords = currentPoints.image;
+
       return true;
     }
 
@@ -75,113 +75,96 @@ export default class CountourFillTool extends BaseBrushTool {
 
       this._drawing = false;
       super._stopListeningForMouseUp(element);
-      this.proceedCalculations(evt);
     }
-
-    proceedCalculations(evt){
-      //calculations
-      console.log(this.startCoords);
-      console.log(this.finishCoords);
-
-      //let arrayPolig = [[[]]];
-     //this.draw(evt, arrayPolig);
-    };
-
-    draw(evt, points){
-    const { labelmap2D, labelmap3D, shouldErase } = this.paintEventData;
-    const columns = this.columns;
- // console.log(points.length);
-    for(let i = 0; i < points.length; ++i ){
-      for (let k = 0; k < points[i].length; ++k) {
-        for (let j = 0; j < points[i][k].length; ++j) {
-          let curPoint = roundPoint(points[i][0][j]);
-          console.log(curPoint);
-          drawBrushPixels(
-            [curPoint],
-            labelmap2D.pixelData,
-            labelmap3D.activeSegmentIndex,
-            columns,
-            shouldErase
-          );
-        }
-      }
-    }
-
-    external.cornerstone.updateImage(evt.detail.element); //*/
-  }
 
     _paint() {
       return null;
     }
-/*
-    renderBrush(){
-    return null;
+
+  mouseDragCallback(evt) {
+    const { currentPoints } = evt.detail;
+
+    this._lastImageCoords = currentPoints.image;
+    this.renderBrush(evt);
   }
-*/
 
   renderBrush(evt) {
-    const {getters, configuration} = segmentationModule;
+    const {getters} = segmentationModule;
     const eventData = evt.detail;
     const viewport = eventData.viewport;
 
     let mousePosition;
 
-    //if (this._drawing) {
-    //    mousePosition = this._lastImageCoords;
-    //} else
-    if (this._mouseUpRender) {
-      mousePosition = this._lastImageCoords;
-      this._mouseUpRender = false;
-    } else {
+    if (this._drawing) {
+      const context = eventData.canvasContext;
+      const element = eventData.element;
+      mousePosition = this._lastImageCoords; //end ellipse point
+      let startPointForDrawing = this.startCoords; //start ellipse point
+      const color = getters.brushColor(element, this._drawing);
+
+      drawEllipse(
+        this.context,
+        element,
+        startPointForDrawing,
+        mousePosition,
+        {
+          color,
+        },
+        'pixel',
+        0.0)
+    }
+    else
+      {
       mousePosition = csTools.store.state.mousePositionImage;
+
+      if (!mousePosition) {
+        return;
+      }
+
+      const { rows, columns } = eventData.image;
+      const { x, y } = mousePosition;
+
+      if (x < 0 || x > columns || y < 0 || y > rows) {
+        return;
+      }
+
+      const radius = 1;
+      const context = eventData.canvasContext;
+      this.context = context;
+      const element = eventData.element;
+      const color = getters.brushColor(element, this._drawing);
+
+      context.setTransform(1, 0, 0, 1, 0, 0);
+
+      let circleRadius = radius * viewport.scale;
+      const mouseCoordsCanvas = window.cornerstone.pixelToCanvas(
+        element,
+        mousePosition,
+      );
+
+      const { labelmap2D } = getters.labelmap2D(element);
+
+      const getPixelIndex = (x, y) => y * columns + x;
+      const spIndex = getPixelIndex(Math.floor(x), Math.floor(y));
+      const isInside = labelmap2D.pixelData[spIndex] === 1;
+      this.shouldErase = !isInside;
+      context.beginPath();
+      context.strokeStyle = color;
+      context.fillStyle = "rgba(128,128,128,0.5)";
+      context.ellipse(
+        mouseCoordsCanvas.x,
+        mouseCoordsCanvas.y,
+        circleRadius,
+        circleRadius,
+        0,
+        0,
+        2 * Math.PI,
+      );
+      context.stroke();
+      context.fill();
+
+      this._lastImageCoords = eventData.image;
     }
-
-    if (!mousePosition) {
-      return null;
-    }
-
-    const {rows, columns} = eventData.image;
-    const {x, y} = mousePosition;
-
-    if (x < 0 || x > columns || y < 0 || y > rows) {
-      return;
-    }
-
-    const radius = 1;
-    const context = eventData.canvasContext;
-    const element = eventData.element;
-    const color = getters.brushColor(element, this._drawing);
-
-    context.setTransform(1, 0, 0, 1, 0, 0);
-
-    let circleRadius = radius * viewport.scale;
-    const mouseCoordsCanvas = window.cornerstone.pixelToCanvas(
-      element,
-      mousePosition,
-    );
-
-    const {labelmap2D} = getters.labelmap2D(element);
-
-    const getPixelIndex = (x, y) => y * columns + x;
-    const spIndex = getPixelIndex(Math.floor(x), Math.floor(y));
-    const isInside = labelmap2D.pixelData[spIndex] === 1;
-    this.shouldErase = !isInside;
-    context.beginPath();
-    context.strokeStyle = color;
-    context.fillStyle = "rgba(128,128,128,0.5)";
-    context.ellipse(
-      mouseCoordsCanvas.x,
-      mouseCoordsCanvas.y,
-      circleRadius,
-      circleRadius,
-      0,
-      0,
-      2 * Math.PI,
-    );
-    context.stroke();
-    context.fill();
-
-    this._lastImageCoords = eventData.image;
   }
 
 }
